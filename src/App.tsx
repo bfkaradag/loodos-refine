36c59750e22907b86005ad17d09c69af95879a6e
loodos <furkan.karadag@loodos.com>
2022-02-24 21:40:12 +0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { Refine } from "@pankod/refine-core";
+import { notificationProvider } from "@pankod/refine-antd";
+import routerProvider from "@pankod/refine-react-router-v6";
+import "@pankod/refine-antd/dist/styles.min.css";
+import dataProvider from "@pankod/refine-simple-rest";
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      notificationProvider={notificationProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+      resources={[
+        {
+          name: "posts",
+          list: PostList,
+          create: PostCreate,
+          edit: PostEdit,
+          show: PostShow,
+        },
+      ]}
+      Title={Title}
+      Header={Header}
+      Sider={Sider}
+      Footer={Footer}
+      Layout={Layout}
+      OffLayoutArea={OffLayoutArea}
+      i18nProvider={i18nProvider}
+    />
+  );
+}
+
+export default App;
